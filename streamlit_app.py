a8815cc0d04e814bbdec9f1775c53a888e3500e4
alexanndre <alexanndre@hotmail.com>
2026-02-22 16:37:04 -0300

    Migrate from HTTP server to Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,4 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import streamlit as st
 
-class HelloHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html; charset=utf-8")
-        self.end_headers()
-        self.wfile.write(b"<h1>Hello, World!</h1>")
-
-if __name__ == "__main__":
-    server_address = ("", 8000)  # porta 8000
-    httpd = HTTPServer(server_address, HelloHandler)
-    print("Servidor rodando em http://localhost:8000")
-    httpd.serve_forever()
+st.title("Hello, World!")
+st.write("Meu primeiro app em Streamlit 🚀")
